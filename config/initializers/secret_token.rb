9bde99aae5eafd8349074e43c227a88d703d3e1a
weyewe <w.yunnal@gmail.com>
2014-06-09 20:22:11 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arap::Application.config.secret_key_base = '1d9b3c6cd57604df50a84824a5c10987521eb87ae9551a060f850aaa00e20a11746e08a6f70f66d4e1713998fbfcc8e7eb66fb93ee65cf4f6a93ba7f4a7602bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arap::Application.config.session_store :cookie_store, key: '_arap_session'
